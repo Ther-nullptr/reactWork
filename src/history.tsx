61d72b181b9bba1046aae52f38d4980c447e4479
Ther <1329438302@qq.com>
2021-07-23 20:38:31 +0800

    add a file

--- src/history.tsx
+++ src/history.tsx
@@ -1,8 +1,36 @@
 import React from "react";
 import { Timeline } from "antd";
 
-const History: React.FC = () => {
-  return <div></div>;
-};
+interface HistoryProps {
+    history: { value: number; status: string }[];
+  }
+  
+  const History: React.FC<HistoryProps> = ({ history }) => {
+    const historyList = history.map((element) => {
+        return (
+          <Timeline.Item
+            key={element.value}
+            color={
+              element.status === "You Win"
+                ? "green"
+                : element.status === "Too small"
+                ? "#ffec3d" // 黄色
+                : "#ffa940" // 橙色
+            }
+          >
+            You've guessed {element.value}. {element.status}
+          </Timeline.Item>
+        );
+      });
+    
+      return (
+        <div style={{ padding: 10 }}>
+          <Timeline>{historyList}</Timeline>
+        </div>
+      );
+    
+  
+  }
 
+  
 export default History;
